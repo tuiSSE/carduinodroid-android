71d9652c4938287c5b2bb77f35299112d08e0e71
buttairfly <till-max.schwikal@tu-ilmenau.de>
2015-12-11 22:34:25 +0100

    update: serial protocol version 2.0 TODO: serial RX protocol update

---
+++ app/src/main/java/tuisse/carduinodroid_android/SerialData.java
@@ -0,0 +1,51 @@
+package tuisse.carduinodroid_android;
+
+import android.util.Log;
+
+/**
+ * Created by keX on 11.12.2015.
+ */
+public class SerialData {
+    private final String TAG = "CarduinoSerialData";
+    //start byte
+    protected final byte startByte = (byte) 0x80;
+    protected final int  bufferLengthOffset = 3;
+    protected final int  checkMask = 0x01;
+    protected final int  checkBit = 7;
+    protected final int  parityMask = 0x80;
+
+    public synchronized static String byteArrayToHexString(byte[] array) {
+        StringBuffer hexString = new StringBuffer();
+        for (byte b : array) {
+            int intVal = b & 0xff;
+            if (intVal < 0x10)
+                hexString.append("0");
+            hexString.append(Integer.toHexString(intVal));
+            hexString.append(" ");
+        }
+        return hexString.toString();
+    }
+
+    public synchronized byte getCheck(byte[] cmd){
+        int length = cmd.length;
+        if(length < bufferLengthOffset){
+            Log.e(TAG, "get Check buffer length to small" + length);
+            return (byte) 0x00;
+        }
+        byte check = 0x00;
+        //calculate xor over all byte in frame
+        for (int i = 0; i < length -1; i++){
+            check ^= cmd[i];
+        }
+        //calculate parity
+        int parity = 0; //even parity
+        for (int i = 0; i < checkBit; i++){
+            if(((check >> i) & checkMask) == checkMask){
+                parity ^= parityMask;
+            }
+        }
+        check &= ~parityMask; //unset bit 7;
+        check |= parity;//set parity bit
+        return check;
+    }
+}
--- app/src/main/java/tuisse/carduinodroid_android/SerialDataTx.java
+++ app/src/main/java/tuisse/carduinodroid_android/SerialDataTx.java
@@ -5,156 +5,122 @@ import android.util.Log;
 /**
  * Created by keX on 07.12.2015.
  */
-public class SerialDataTx {
+public class SerialDataTx extends SerialData {
     private final String TAG = "CarduinoSerialDataTx";
-    private final int bufferLength = 4;
-    //byte 0
-    private final int version = 1;
-    private final int length = bufferLength - 1;
-    private final int versionShift = 6;
-    private final int versionMask = 0xc0;
+
+    //byte 0 Start
+    //byte 1 Version+Length
+    private final int version = 2;
+    private final int length = 3;
+    private final int versionShift = 4;
+    private final int versionMask = 0x70;
     private final int lengthMask = 0x0f;
-    private final int speedValShift = 4;
-    private final int speedValMask = 0xf0;
-    private final int speedValMax = 9;
-    private final int speedValMin = 0;
-    private final int speedDirShift = 3;
-    private final int speedDirMask = 0x08;
-    private final int steerValShift = 4;
-    private final int steerValMask = 0xf0;
-    private final int steerValMax = 0x8;
-    private final int steerValMin = 0x0;
-    private final int steerDirShift = 3;
-    private final int steerDirMask = 0x08;
-    private final int statusLedShift = 7;
-    private final int statusLedMask = 0x80;
-    private final int frontLightShift = 6;
-    private final int frontLightMask = 0x40;
-    private final int failSafeStopShift = 5;
-    private final int failSafeStopMask = 0x20;
+    private final int bufferLength = length + bufferLengthOffset;
+    //byte 2 Speed
+    private final int speedMax = 127;//forwards
+    private final int speedMin = -127;//backwards
+    //byte 3 Steer
+    private final int steerMax = 127;//right
+    private final int steerMin = -127;//left
+    //byte 4
+    private final int statusLedShift = 0;
+    private final int statusLedMask = 1 << statusLedShift;
+    private final int frontLightShift = 1;
+    private final int frontLightMask = 1 << frontLightShift;
     private final int resetAccCurShift = 4;
-    private final int resetAccCurMask = 0x10;
-    //byte 1
-    private int speedVal;
-    private int speedDir;
-    //byte 2
-    private int steerVal;
-    private int steerDir;
-    //byte 3
+    private final int resetAccCurMask = 1 << resetAccCurShift;
+    private final int failSafeStopShift = 5;
+    private final int failSafeStopMask = 1 << failSafeStopShift;
+
+    private int speed;
+    private int steer;
     private int statusLed;
     private int frontLight;
     private int failSafeStop;
     private int resetAccCur;
 
     public SerialDataTx(){
-        speedVal = 4;
-        speedDir = 1;
-        //byte 2
-        steerVal = 0;
-        steerDir = 0;
-        //byte 3
+        speed = 15;
+        steer = -10;
         statusLed = 0;
         frontLight = 1;
-        failSafeStop = 1;
         resetAccCur = 0;
-    }
-
-    public synchronized static String byteArrayToHexString(byte[] array) {
-        StringBuffer hexString = new StringBuffer();
-        for (byte b : array) {
-            int intVal = b & 0xff;
-            if (intVal < 0x10)
-                hexString.append("0");
-            hexString.append(Integer.toHexString(intVal));
-            hexString.append(" ");
-        }
-        return hexString.toString();
+        failSafeStop = 1;
     }
 
     public synchronized String print(){
         return  "version      "+ version+
                 "length       "+ length+
-                "speedVal     "+ speedVal+
-                "speedDir     "+ speedDir+
-                "steerVal     "+ steerVal+
-                "steerDir     "+ steerDir+
+                "speedVal     "+ speed+
+                "steerVal     "+ steer+
                 "statusLed    "+ statusLed+
                 "frontLight   "+ frontLight+
                 "failSafeStop "+ failSafeStop+
                 "resetAccCur  "+ resetAccCur;
+
     }
 
     public synchronized byte[] get() {
         byte[] command = new byte[bufferLength];
-        command[0] = (byte) (0x00 | ((length) & lengthMask)
-                | ((version << versionShift) & versionMask));
-        command[1] = (byte) (0x00 | ((speedDir << speedDirShift) & speedDirMask)
-                | ((speedVal << speedValShift) & speedValMask));
-        command[2] = (byte) (0x00 | ((steerDir << steerDirShift) & steerDirMask)
-                | ((steerVal << steerValShift) & steerValMask));
-        command[3] = (byte) (0x00 | ((statusLed << statusLedShift) & statusLedMask)
-                | ((frontLight << frontLightShift) & frontLightMask)
-                | ((failSafeStop << failSafeStopShift) & failSafeStopMask)
-                | ((resetAccCur << resetAccCurShift) & resetAccCurMask));
+        command[0] = startByte;
+        command[1] = getVersionLength();
+        command[2] = getSpeed();
+        command[3] = getSteer();
+        command[4] = getStatus();
+        command[5] = getCheck(command);
         //Log.d(TAG,print());
         //Log.d(TAG,byteArrayToHexString(command));
         return command;
     }
 
-    public synchronized void setSpeed(int speed) {
-        if ((speed < -speedValMax) || (speed > speedValMax))
-            Log.e(TAG, "setSpeed out of bounds" + speed);
+    public synchronized byte getVersionLength(){
+        if(version == 15) {
+            Log.e(TAG, "version must not be 15");
+            return (byte) 0x00;
+        }
+        return  (byte) (0x00 | ((length) & lengthMask) | ((version << versionShift) & versionMask));
+    }
+
+    public synchronized byte getSpeed(){
+        if((byte) speed == startByte){
+            Log.e(TAG, "speed must not be -128");
+            speed = 0;
+        }
+        return (byte) speed;
+    }
+
+    public synchronized byte getSteer(){
+        if((byte) steer == startByte){
+            Log.e(TAG, "steer must not be -128");
+            steer = 0;
+        }
+        return (byte) steer;
+    }
+
+    public synchronized byte getStatus(){
+        return (byte) (0x00
+                | ((statusLed << statusLedShift) & statusLedMask)
+                | ((frontLight << frontLightShift) & frontLightMask)
+                | ((failSafeStop << failSafeStopShift) & failSafeStopMask)
+                | ((resetAccCur << resetAccCurShift) & resetAccCurMask));
+    }
+
+    public synchronized void setSpeed(int _speed) {
+        if ((_speed < speedMin) || (_speed > speedMax)) {
+            Log.e(TAG, "setSpeed out of bounds" + _speed);
+        }
         else {
-            if (speed < speedValMin) {
-                this.speedDir = 0; //backwards
-                this.speedVal = -speed; // speed is negative
-            } else {
-                this.speedDir = 1; //forwards
-                this.speedVal = speed;
-            }
+            speed = _speed;
         }
     }
 
-    public synchronized void setSpeedVal(int speedVal) {
-        if ((speedVal >= speedValMin) && (speedVal <= speedValMax))
-            this.speedVal = speedVal;
-        else
-            Log.e(TAG, "setSpeedVal out of bounds" + speedVal);
-    }
-
-    public synchronized void setSpeedDir(int speedDir) {
-        if (speedDir != 0)
-            this.speedDir = 1;//forwards
-        else
-            this.speedDir = 0;//backwards
-    }
-
-    public synchronized void setSteer(int steer) {
-        if ((steer < -steerValMax) || (steer > steerValMax))
-            Log.e(TAG, "setSteer out of bounds" + steer);
-        else {
-            if (steer < steerValMin) {
-                this.steerDir = 0; //left
-                this.steerVal = -steer; // steer is negative
-            } else {
-                this.steerDir = 1; //right
-                this.steerVal = steer;
-            }
+    public synchronized void setSteer(int _steer) {
+        if ((_steer < steerMin) || (_steer > steerMax)) {
+            Log.e(TAG, "setSteer out of bounds" + _steer);
         }
-    }
-
-    public synchronized void setSteerVal(int steerVal) {
-        if ((steerVal >= steerValMin) && (steerVal <= steerValMax))
-            this.steerVal = steerVal;
-        else
-            Log.e(TAG, "setSteerVal out of bounds" + steerVal);
-    }
-
-    public synchronized void setSteerDir(int steerDir) {
-        if (steerDir != 0)
-            this.steerDir = 1;//right
-        else
-            this.steerDir = 0;//left
+        else {
+            steer = _steer;
+        }
     }
 
     public synchronized void setStatusLed(int statusLed) {
@@ -171,17 +137,17 @@ public class SerialDataTx {
             this.frontLight = 0;//off = default
     }
 
-    public synchronized void setFailSafeStop(int failSafeStop) {
-        if (failSafeStop != 0)
-            this.failSafeStop = 1;//on = default
-        else
-            this.failSafeStop = 0;//off
-    }
-
     public synchronized void setResetAccCur(int resetAccCur) {
         if (resetAccCur != 0)
             this.resetAccCur = 1;//on
         else
             this.resetAccCur = 0;//off = default
+    }
+
+    public synchronized void setFailSafeStop(int failSafeStop) {
+        if (failSafeStop != 0)
+            this.failSafeStop = 1;//on = default
+        else
+            this.failSafeStop = 0;//off
     }
 }
