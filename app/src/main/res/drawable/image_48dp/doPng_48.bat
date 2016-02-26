ec43ef803e1657a3b8144029807249019de95391
buttairfly <schkaty@web.de>
2016-02-26 11:27:54 +0100

    added icons, setup ipdataCounter

--- app/src/main/res/drawable/image_48dp/doPng_48.bat
+++ app/src/main/res/drawable/image_48dp/doPng_48.bat
@@ -8,12 +8,13 @@ SET toDirXHDPI=%toDir%\..\%draw%-xhdpi
 SET toDirXXHDPI=%toDir%\..\%draw%-xxhdpi
 SET toDirXXXHDPI=%toDir%\..\%draw%-xxxhdpi
 
-cd "C:\Program Files\Inkscape\"
+#cd "C:\Program Files\Inkscape\"
+cd "C:\Program Files (x86)\Inkscape\"
 
 for /f "delims=" %%f in ('dir /b /a-d-h-s %dir%\*.svg') do (inkscape %dir%%%f --export-png=%toDir%\%%~nf.png)
 
 for /f "delims=" %%f in ('dir /b /a-d-h-s %dir%\*.svg') do (inkscape %dir%%%f --export-png=%toDirMDPI%\%%~nf.png --export-height=48)
-#for /f "delims=" %%f in ('dir /b /a-d-h-s %dir%\*.svg') do (inkscape %dir%%%f --export-png=%toDirHDPI%\%%~nf.png --export-height=72)
-#for /f "delims=" %%f in ('dir /b /a-d-h-s %dir%\*.svg') do (inkscape %dir%%%f --export-png=%toDirXHDPI%\%%~nf.png --export-height=96)
-#for /f "delims=" %%f in ('dir /b /a-d-h-s %dir%\*.svg') do (inkscape %dir%%%f --export-png=%toDirXXHDPI%\%%~nf.png --export-height=144)
-#for /f "delims=" %%f in ('dir /b /a-d-h-s %dir%\*.svg') do (inkscape %dir%%%f --export-png=%toDirXXXHDPI%\%%~nf.png --export-height=192)
+for /f "delims=" %%f in ('dir /b /a-d-h-s %dir%\*.svg') do (inkscape %dir%%%f --export-png=%toDirHDPI%\%%~nf.png --export-height=72)
+for /f "delims=" %%f in ('dir /b /a-d-h-s %dir%\*.svg') do (inkscape %dir%%%f --export-png=%toDirXHDPI%\%%~nf.png --export-height=96)
+for /f "delims=" %%f in ('dir /b /a-d-h-s %dir%\*.svg') do (inkscape %dir%%%f --export-png=%toDirXXHDPI%\%%~nf.png --export-height=144)
+for /f "delims=" %%f in ('dir /b /a-d-h-s %dir%\*.svg') do (inkscape %dir%%%f --export-png=%toDirXXXHDPI%\%%~nf.png --export-height=192)
